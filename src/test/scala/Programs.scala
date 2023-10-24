c298b93d1ec923a3315f2ccb4dfd67a19c67341d
tacecapSx <martin.handest@gmail.com>
2023-10-24 15:36:26 +0200

    assembly program compiled as program1

--- src/test/scala/Programs.scala
+++ src/test/scala/Programs.scala
@@ -6,18 +6,45 @@ import java.util
 
 object Programs{
   val program1 = Array(
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W),
-    "h00000000".U(32.W)
+    "hFFEC0003".U(32.W), // li a0, -20
+    "h00000023".U(32.W), // li a2, 0
+    "h00000013".U(32.W), // li a1, 0
+    "h00140332".U(32.W), // addi a0, a0, 20
+    "hFE700042".U(32.W), // addi t0, a0, -400
+    "h001F0046".U(32.W), // beqz t0, end (inst. 31)
+    "h00000244".U(32.W), // lb t0, 0(a2)
+    "h00000432".U(32.W), // addi a3, t0, 0
+    "h001D0046".U(32.W), // beqz t0, set_zero (inst. 29)
+    // check rows
+    "h001D0006".U(32.W), // beqz a0, set_zero (inst. 29)
+    "hFFED0042".U(32.W), // addi t0, a0, -19
+    "h001D0046".U(32.W), // beqz t0, set_zero (inst. 29)
+    // check columns
+    "h001D0016".U(32.W), // beqz a1, set_zero (inst. 29)
+    "hFFED0142".U(32.W), // addi t0, a1, -19
+    "h001D0046".U(32.W), // beqz t0, set_zero (inst. 29)
+    // right neighbor
+    "h00010244".U(32.W), // lb t0, 1(a2)
+    "h001D0046".U(32.W), // beqz t0, set_zero (inst. 29)
+    // left neighbor
+    "hFFFF0244".U(32.W), // lb t0, -1(a2)
+    "h001D0046".U(32.W), // beqz t0, set_zero (inst. 29)
+    // above neighbor
+    "hFFEC0244".U(32.W), // lb t0, -20(a2)
+    "h001D0046".U(32.W), // beqz t0, set_zero (inst. 29)
+    // below neighbor
+    "h00140244".U(32.W), // lb t0, 20(a2)
+    "h001D0046".U(32.W), // beqz t0, set_zero (inst. 29)
+
+    "h01900035".U(32.W), // sb a3, 400(a2)
+    "h00010222".U(32.W), // addi a2, a2, 1
+    "h00010112".U(32.W), // addi a1, a1, 1
+    "hFFEC0142".U(32.W), // addi t0, a1, -20
+    "h00020046".U(32.W), // beqz t0, outer_loop (inst. 2)
+    "h00060007".U(32.W), // j inner_loop (inst. 6)
+    "h00000033".U(32.W), // li a3, 0
+    "h00170007".U(32.W), // j next (inst. 23)
+    "h00000008".U(32.W)  // end
   )
 
   val program2 = Array(
