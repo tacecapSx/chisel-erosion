48044ad697dc31a9cfe769ea5837e235e5956a74
pandekagee <oskarwholland@gmail.com>
2023-10-25 15:05:22 +0200

    ControlUnit

--- src/main/scala/ALU.scala
+++ src/main/scala/ALU.scala
@@ -6,16 +6,13 @@ class ALU extends Module {
     //Define the module interface here (inputs/outputs)
     val a = Input(UInt(16.W))
     val b = Input(UInt(16.W))
-    val fn = Input(Bool()) // Control input for the condition
+    val fn = Input(Bool())
     val y = Output(UInt(16.W))
   })
 
-  // Perform addition
   val addition = io.a + io.b
 
-  // Check the condition (x == 0)
   val eqZero = io.fn && (io.a === 0.U)
 
-  // Output the result based on the condition
   io.y := Mux(eqZero, 0.U, addition)
 }
--- src/main/scala/ControlUnit.scala
+++ src/main/scala/ControlUnit.scala
@@ -3,9 +3,74 @@ import chisel3.util._
 
 class ControlUnit extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
+    val instruction = Input(UInt(32.W))
+    val opcode = Output(UInt(4.W))
+    val regA = Output(UInt(4.W))
+    val regB = Output(UInt(4.W))
+    val regC = Output(UInt(4.W))
+    val immediate = Output(UInt(16.W))
+    val aluOp = Output(Bool())
+    val memRead = Output(Bool())
+    val memWrite = Output(Bool())
+    val branch = Output(Bool())
+    val jump = Output(Bool())
+    val end = Output(Bool())
   })
+  
+  val opcode = io.instruction(31, 28)
+  val regA = io.instruction(27, 24)
+  val regB = io.instruction(23, 20)
+  val regC = io.instruction(19, 16)
+  val immediate = io.instruction(15, 0)
 
-  //Implement this module here
+  io.opcode := opcode
+  io.regA := regA
+  io.regB := regB
+  io.regC := regC
+  io.immediate := immediate
+  io.aluOp := false.B
+  io.memRead := false.B
+  io.memWrite := false.B
+  io.branch := false.B
+  io.jump := false.B
+  io.end := false.B
 
+  switch(opcode) {
+    is("b0000".U) {
+      // No operation (nop)
+      io.end := true.B
+    }
+    is("b0001".U) {
+      // Addition
+      io.aluOp := true.B
+    }
+    is("b0010".U) {
+      // Immediate addition
+      io.aluOp := true.B
+    }
+    is("b0011".U) {
+      // Load immediate
+      io.memRead := true.B
+    }
+    is("b0100".U) {
+      // Load byte
+      io.memRead := true.B
+    }
+    is("b0101".U) {
+      // Save byte
+      io.memWrite := true.B
+    }
+    is("b0110".U) {
+      // Branch if equals zero
+      io.branch := true.B
+    }
+    is("b0111".U) {
+      // Jump
+      io.jump := true.B
+    }
+    is("b1000".U) {
+      // End execution
+      io.end := true.B
+    }
+  }
 }
